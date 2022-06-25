330def31303b9e4d471289117a0848601928fdde
Daniel López <danieljlopez@udec.cl>
2022-06-24 22:54:17 -0400

    update code

--- main.cpp
+++ main.cpp
@@ -4,33 +4,46 @@
 
 using namespace std;
 
-class Graph;
-
-int main(int argc, char const *argv[])
-{
-    int n = 0; // number of vertices
-    int m = 0; // number of edges
-    Graph *graph = new Graph(n);
-
-    cout << "Hi mom!" << endl;
-    return 0;
-}
-
 class Graph
 {
 private:
     int n; // number of vertices
     int m; // number of edges
 public:
-    Graph(int);
-    ~Graph();
+    Graph(int n)
+    {
+        this->n = n;
+    }
+    ~Graph()
+    {
+    }
+
+    // Agregar una arista dirigida desde u hacia v
+    void addEdge(int u, int v)
+    {
+    }
+
+    // Verificar a través de un recorrido DFS (implementado usando stack)
+    // si el nodo u puede alcanzar a todos los otros nodos:
+    bool checkConnectivityDFS(int u)
+    {
+    }
+
+    // Verificar a través de un recorrido BFS (implementado usando queue)
+    // si el nodo u puede alcanzar a todos los otros nodos:
+    bool checkConnectivityBFS(int u)
+    {
+    }
 };
 
-Graph::Graph(int n)
+int main(int argc, char const *argv[])
 {
-    this->n = n;
-}
+    int n = 0;
+    int m = 0;
+    Graph *graph = new Graph(n);
 
-Graph::~Graph()
-{
+    cout << "Hi mom!" << endl;
+
+    delete graph;
+    return 0;
 }
